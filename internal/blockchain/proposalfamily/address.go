6c9a2d620b4dcff786ef18ae59cb72a96fdeda76
anna <anjankow@protonmail.com>
2022-06-06 16:32:16 -0400

    move consts to family file

--- internal/blockchain/proposalfamily/address.go
+++ internal/blockchain/proposalfamily/address.go
@@ -6,19 +6,6 @@ import (
 	"sync"
 )
 
-const (
-	FamilyName    string = "proposals"
-	FamilyVersion string = "1.0"
-
-	// to hold all proposal related data
-	proposalDataPrefix = "proposaldata"
-	// to hold proposalIDs of active and accepted proposals created by the user
-	// and proposalIDs he was voting on
-	userPrefix = "user"
-	// to hold proposal IDs of active proposals for the doc
-	docPrefix = "doc"
-)
-
 var (
 	familyHash             = ""
 	proposalDataPrefixHash = ""
--- internal/blockchain/proposalfamily/family.go
+++ internal/blockchain/proposalfamily/family.go
@@ -7,3 +7,16 @@ const (
 	ActionVote   Action = "vote"
 	ActionDelete Action = "delete"
 )
+
+const (
+	FamilyName    string = "proposals"
+	FamilyVersion string = "1.0"
+
+	// to hold all proposal related data
+	proposalDataPrefix = "proposaldata"
+	// to hold proposalIDs of active and accepted proposals created by the user
+	// and proposalIDs he was voting on
+	userPrefix = "user"
+	// to hold proposal IDs of active proposals for the doc
+	docPrefix = "doc"
+)
